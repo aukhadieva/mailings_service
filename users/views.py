2570f5cb654b2480b1b520227e064c09ccb00146
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-07 15:58:06 +0500

    Add FBV

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def sign_in(request):
+    return render(request, 'users/sign_in.html')
+
+
+def sign_up(request):
+    return render(request, 'users/sign_up.html')
