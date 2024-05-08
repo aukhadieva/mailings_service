6409bad709c06762167b8824fae8f631f70757e1
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-08 16:53:24 +0500

    Add url for FBV profile

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import sign_in, sign_up
+from users.views import sign_in, sign_up, profile
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('signin/', sign_in, name='sign_in'),
     path('signup/', sign_up, name='sign_up'),
+    path('profile/', profile, name='profile')
 ]
