d648b2ebdd4c8a2c6fa83cee0906df9ef727e436
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-08 12:03:54 +0500

    Update FBV for login and registration

--- users/views.py
+++ users/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def sign_in(request):
-    return render(request, 'users/sign_in.html')
+    """
+    Авторизация и аутентификация пользователя.
+    """
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/sign_in.html', context)
 
 
 def sign_up(request):
-    return render(request, 'users/sign_up.html')
+    """
+    Регистрация пользователя.
+    """
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:sign_in'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/sign_up.html', context)
