c8d5e1f705080d3f18c68e69cb8b866efc39ed1b
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-09 20:49:14 +0500

    Add CBV ClientCreateView

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from mailing.forms import ClientForm
+from mailing.models import Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('main:index')
