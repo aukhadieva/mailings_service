78f4ca2bf4fc10a146ac45afc63f55c320f2adab
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-09 20:43:24 +0500

    Add Client model

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    """Модель клиента, получающего рассылки."""
+    full_name = models.CharField(max_length=500, verbose_name='Ф.И.О.')
+    email = models.EmailField(verbose_name='контактный email')
+    comment = models.CharField(max_length=500, verbose_name='комментарий', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.full_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
