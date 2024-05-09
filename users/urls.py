29d04a8d78c3e535adbc391d87fe5be1830c8103
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-09 07:15:59 +0500

    Add url for logout

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import sign_in, sign_up, profile
+from users.views import sign_in, sign_up, profile, logout
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('signin/', sign_in, name='sign_in'),
     path('signup/', sign_up, name='sign_up'),
-    path('profile/', profile, name='profile')
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout')
 ]
