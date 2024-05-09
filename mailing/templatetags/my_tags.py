850832f759b155ced1819af8014de73de90df895
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-09 20:51:26 +0500

    Add custom tag for ClientForm

---
+++ mailing/templatetags/my_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+from mailing.forms import ClientForm
+
+register = template.Library()
+
+
+@register.inclusion_tag('mailing/client_form.html')
+def client_form():
+    return {'client_form': ClientForm()}
