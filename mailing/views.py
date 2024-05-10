b91b075da59ec13186b97273a7d0ff846e70be82
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-10 09:46:17 +0500

    Add CBV for MailingMessage

--- mailing/views.py
+++ mailing/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 
-from mailing.forms import ClientForm
-from mailing.models import Client
+from mailing.forms import ClientForm, MailingMessageForm
+from mailing.models import Client, MailingMessage
 from utils import TitleMixin
 
 
@@ -39,3 +40,38 @@ class ClientUpdateView(TitleMixin, UpdateView):
     def get_success_url(self):
         client = self.get_object()
         return reverse('mailing:view_client', args=[client.pk])
+
+
+class MailingMessageCreateView(TitleMixin, CreateView):
+    model = MailingMessage
+    form_class = MailingMessageForm
+    success_url = reverse_lazy('mailing:message_list')
+    title = 'Создание сообщения'
+
+
+class MailingMessageListView(TitleMixin, ListView):
+    model = MailingMessage
+    title = 'Список сообщений'
+
+
+class MailingMessageDetailView(TitleMixin, DetailView):
+    model = MailingMessage
+
+    def get_title(self):
+        return self.object.title
+
+
+class MailingMessageUpdateView(TitleMixin, UpdateView):
+    model = MailingMessage
+    form_class = MailingMessageForm
+    title = 'Редактирование сообщения'
+
+    def get_success_url(self):
+        message = self.get_object()
+        return reverse('mailing:view_message', args=[message.pk])
+
+
+class MailingMessageDeleteView(TitleMixin, DeleteView):
+    model = MailingMessage
+    success_url = reverse_lazy('mailing:message_list')
+    title = 'Удаление сообщения'
