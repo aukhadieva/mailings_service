07139c1e724f8eebf7cd5e4e8856b513990726d4
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-10 06:33:34 +0500

    Add title

--- mailing/views.py
+++ mailing/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 
 from mailing.forms import ClientForm
@@ -9,13 +9,17 @@ from utils import TitleMixin
 class ClientCreateView(TitleMixin, CreateView):
     model = Client
     form_class = ClientForm
-    success_url = reverse_lazy('main:index')
+    success_url = reverse_lazy('mailing:client_list')
     title = 'Создание клиента'
 
 
 class ClientListView(TitleMixin, ListView):
     model = Client
+    title = 'Список клиентов'
 
 
 class ClientDetailView(TitleMixin, DetailView):
     model = Client
+
+    def get_title(self):
+        return self.object.full_name
