b0cc5827a2af39a12ca248adcf12e6d858e88679
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-10 07:46:15 +0500

    Add MailingMessage and Mailing models

--- mailing/models.py
+++ mailing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from users.models import User
 
 NULLABLE = {'blank': True, 'null': True}
 
@@ -16,3 +17,34 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'клиент'
         verbose_name_plural = 'клиенты'
+
+
+class MailingMessage(models.Model):
+    """Модель письма в рассылке."""
+    title = models.CharField(max_length=150, verbose_name='тема письма')
+    body = models.TextField(verbose_name='тело письма')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'письмо'
+        verbose_name_plural = 'письма'
+
+
+class Mailing(models.Model):
+    """Модель рассылки."""
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='менеджер рассылки')
+    title = models.CharField(max_length=300, verbose_name='название рассылки')
+    created_datetime = models.DateTimeField(auto_now_add=True, verbose_name='дата и время первой отправки рассылки')
+    period = models.CharField(max_length=50, verbose_name='периодичность рассылки')
+    status = models.CharField(max_length=50, verbose_name='статус рассылки')
+    target = models.ManyToManyField(Client, verbose_name='получатели рассылки')
+    message = models.ForeignKey(MailingMessage, on_delete=models.CASCADE, verbose_name='сообщение рассылки')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
