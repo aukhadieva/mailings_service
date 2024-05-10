925138657482159765193926de807894d5c89b2c
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-10 09:46:44 +0500

    Add urls for MailingMessage CBV

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import ClientCreateView, ClientListView, ClientDetailView, ClientDeleteView, ClientUpdateView
+from mailing.views import (ClientCreateView, ClientListView, ClientDetailView, ClientDeleteView, ClientUpdateView,
+                           MailingMessageCreateView, MailingMessageListView, MailingMessageDetailView,
+                           MailingMessageDeleteView, MailingMessageUpdateView)
 
 app_name = MailingConfig.name
 
@@ -10,5 +12,10 @@ urlpatterns = [
     path('client_list/', ClientListView.as_view(), name='client_list'),
     path('view_client/<int:pk>', ClientDetailView.as_view(), name='view_client'),
     path('delete_client/<int:pk>', ClientDeleteView.as_view(), name='delete_client'),
-    path('edite_client/<int:pk>', ClientUpdateView.as_view(), name='edite_client'),
+    path('edit_client/<int:pk>', ClientUpdateView.as_view(), name='edit_client'),
+    path('create_message', MailingMessageCreateView.as_view(), name='create_message'),
+    path('message_list/', MailingMessageListView.as_view(), name='message_list'),
+    path('view_message/<int:pk>', MailingMessageDetailView.as_view(), name='view_message'),
+    path('delete_message/<int:pk>', MailingMessageDeleteView.as_view(), name='delete_message'),
+    path('edit_message/<int:pk>', MailingMessageUpdateView.as_view(), name='edit_message'),
 ]
