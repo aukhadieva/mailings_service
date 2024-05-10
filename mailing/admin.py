3011f663887a9eb6ea997e231b0c94523407da86
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-10 07:24:45 +0500

    Fix admin.py

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from mailing.models import Client
+
+
+@admin.register(Client)
+class AdminClient(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email',)
+    search_fields = ('full_name',)
--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from mailing.models import Client
-
-
-@admin.register(Client)
-class AdminClient(admin.ModelAdmin):
-    list_display = ('id', 'full_name', 'email',)
-    search_fields = ('full_name',)
+# Register your models here.
