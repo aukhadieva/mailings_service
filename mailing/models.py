6ea184523df3285090bc0476bda2935a7e708518
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-11 20:47:19 +0500

    Update MailingLog model

--- mailing/models.py
+++ mailing/models.py
@@ -3,6 +3,21 @@ from django.db import models
 from users.models import User
 
 NULLABLE = {'blank': True, 'null': True}
+LOGS_STATUS_CHOICES = [
+    ('success', 'успешно'),
+    ('fail', 'неуспешно'),
+]
+PERIOD_CHOICES = [
+    ('once', 'один раз'),
+    ('daily', 'ежедневно'),
+    ('weekly', 'еженедельно'),
+    ('monthly', 'ежемесячно'),
+]
+MAILING_STATUS_CHOICES = [
+    ('created', 'создана'),
+    ('completed', 'завершена'),
+    ('launched', 'запущена'),
+]
 
 
 class Client(models.Model):
@@ -34,24 +49,12 @@ class MailingMessage(models.Model):
 
 class Mailing(models.Model):
     """Модель рассылки."""
-    PERIOD_CHOICES = [
-        ('once', 'один раз'),
-        ('daily', 'ежедневно'),
-        ('weekly', 'еженедельно'),
-        ('monthly', 'ежемесячно'),
-    ]
-
-    STATUS_CHOICES = [
-        ('created', 'создана'),
-        ('completed', 'завершена'),
-        ('launched', 'запущена'),
-    ]
-
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='менеджер рассылки')
     title = models.CharField(max_length=300, verbose_name='название рассылки')
     created_datetime = models.DateTimeField(auto_now_add=True, verbose_name='дата и время первой отправки рассылки')
     period = models.CharField(max_length=50, choices=PERIOD_CHOICES, default='', verbose_name='периодичность рассылки')
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='created', verbose_name='статус рассылки')
+    status = models.CharField(max_length=50, choices=MAILING_STATUS_CHOICES, default='created',
+                              verbose_name='статус рассылки')
     target = models.ManyToManyField(Client, verbose_name='получатели рассылки')
     message = models.ForeignKey(MailingMessage, on_delete=models.CASCADE, verbose_name='сообщение рассылки')
 
@@ -65,13 +68,9 @@ class Mailing(models.Model):
 
 class MailingLog(models.Model):
     """Модель логов рассылки."""
-    STATUS_CHOICES = [
-        ('success', 'успешно'),
-        ('fail', 'неуспешно'),
-    ]
-
-    datetime_last_attempt = models.DateTimeField(auto_now=True, editable=False, verbose_name='дата и время последней попытки')
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='', verbose_name='статус попытки')
+    datetime_last_attempt = models.DateTimeField(auto_now=True, editable=False,
+                                                 verbose_name='дата и время последней попытки')
+    status = models.CharField(max_length=50, choices=LOGS_STATUS_CHOICES, default='', verbose_name='статус попытки')
     server_response = models.CharField(max_length=500, verbose_name='ответ почтового сервера', **NULLABLE)
     mailing_id = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name='рассылка')
 
@@ -79,5 +78,5 @@ class MailingLog(models.Model):
         return self.status
 
     class Meta:
-        verbose_name = 'попытка'
-        verbose_name_plural = 'попытки'
+        verbose_name = 'попытка (логи рассылки)'
+        verbose_name_plural = 'попытки (логи рассылки)'
