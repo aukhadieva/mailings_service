b1666945653c45f7a75cb174b7a1ceb924ba747f
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-22 20:08:53 +0500

    Update views

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import sign_in, sign_up, profile, logout
+from users.views import UserLoginView, UserRegisterView, UserProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('signin/', sign_in, name='sign_in'),
-    path('signup/', sign_up, name='sign_up'),
-    path('profile/', profile, name='profile'),
-    path('logout/', logout, name='logout')
+    path('', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
