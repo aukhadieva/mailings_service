44b470882c9ece1594c9c59f1d70d521fa6dc6b5
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-29 12:13:59 +0500

    Add url for EmailVerificationView

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserLoginView, UserRegisterView, UserProfileView
+from users.views import UserLoginView, UserRegisterView, UserProfileView, EmailVerificationView
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('verify/<str:email>/<uuid:key>', EmailVerificationView.as_view(), name='verify')
 ]
