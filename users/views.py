c73cc10bc09216b4cba5371d2c33b23ae3a7ad6f
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-29 21:51:21 +0500

    Update get method (EmailVerificationView)

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView, TemplateView
@@ -50,8 +51,10 @@ class EmailVerificationView(TitleMixin, TemplateView):
         key = kwargs['key']
         user = User.objects.get(email=kwargs['email'])
         email_verifications = EmailVerification.objects.filter(user=user, key=key)
+        group = Group.objects.get(name='Mailing owner')
         if email_verifications.exists() and not email_verifications.first().is_expired():
             user.is_active = True
+            user.groups.add(group)
             user.save()
             return super().get(request, *args, **kwargs)
         else:
