2faecf592a9a947a6ba0c2b5e103d9075c636604
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-29 10:18:51 +0500

    Update User model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to='users_images', blank=True, null=True)
+    username = None
+    email = models.EmailField(unique=True, verbose_name='электронная почта')
+    image = models.ImageField(upload_to='users_images/', blank=True, null=True, verbose_name='аватар')
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
