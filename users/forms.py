c43e15d49b7a75de9e530f3412c459ca5316c227
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-29 12:16:42 +0500

    Override save method (UserRegistrationForm)

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,11 @@
+import uuid
+from datetime import timedelta
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.utils.timezone import now
 
-from users.models import User
+from users.models import User, EmailVerification
 from utils import StyleMixin
 
 
@@ -23,6 +27,14 @@ class UserRegistrationForm(StyleMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2',)
+
+    def save(self, commit=True):
+        user = super().save(commit=True)
+        key = uuid.uuid4()
+        expiration = now() + timedelta(hours=24)
+        verification = EmailVerification.objects.create(user=user, key=key, expiration=expiration)
+        verification.send_verification_email()
+        return user
 
 
 class UserProfileForm(StyleMixin, UserChangeForm):
