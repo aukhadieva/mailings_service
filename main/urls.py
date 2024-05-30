a914bcfbe161c2bcf50621f6295e709028d5c20c
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-30 17:38:08 +0500

    Add MainTemplateView

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index
+from main.views import MainTemplateView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', MainTemplateView.as_view(), name='home_page')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from blog.models import BlogPost
+from mailing.models import Mailing, Client
+from utils import TitleMixin
 
 
-def index(request):
-    return render(request, 'main/index.html')
+class MainTemplateView(TitleMixin, TemplateView):
+    title = 'Главная'
+    template_name = 'main/home_page.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Возвращает данные контекста для отображения списка объектов.
+        """
+        context_data = super().get_context_data(**kwargs)
+        context_data['posts'] = BlogPost.objects.all()
+        context_data['mailings'] = Mailing.objects.all()
+        context_data['clients'] = Client.objects.all()
+        return context_data
