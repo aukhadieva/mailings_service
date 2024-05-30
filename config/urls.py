b81f9d0d55b067da4af41a1ce96342d031186261
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-30 12:14:29 +0500

    Add blog.urls

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('', include('main.urls', namespace='main')),
-    path('mailing/', include('mailing.urls', namespace='mailing'))
+    path('mailing/', include('mailing.urls', namespace='mailing')),
+    path('blog/', include('blog.urls', namespace='blog'))
 ]
