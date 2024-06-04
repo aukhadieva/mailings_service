89add4a573fcf535ac27b94994a3fd4b76cbea33
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-04 10:16:06 +0500

    Override get_context_data (MainTemplateView)

--- main/views.py
+++ main/views.py
@@ -14,7 +14,15 @@ class MainTemplateView(TitleMixin, TemplateView):
         Возвращает данные контекста для отображения списка объектов.
         """
         context_data = super().get_context_data(**kwargs)
-        context_data['posts'] = BlogPost.objects.all()
-        context_data['mailings'] = Mailing.objects.all()
-        context_data['clients'] = Client.objects.all()
+
+        context_data['posts'] = BlogPost.objects.filter(is_published=True)[:4]
+
+        mailings = Mailing.objects.all()
+        context_data['mailings'] = len(mailings)
+
+        active_mailings = Mailing.objects.filter(status='launched')
+        context_data['active_mailings'] = len(active_mailings)
+
+        clients = Client.objects.all()
+        context_data['clients'] = len(clients)
         return context_data
