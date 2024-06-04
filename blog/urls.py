ec8106ac3a79a609527b4bc985af75b8ec6abb72
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-04 10:21:49 +0500

    Add CRUD for BlogPost model

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from blog.apps import BlogConfig
+from blog.views import BlogPostCreateView, BlogPostUpdateView, BlogPostDetailView, BlogPostDeleteView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('create_post', BlogPostCreateView.as_view(), name='create_post'),
+    path('edit_post/<int:pk>', BlogPostUpdateView.as_view(), name='edit_post'),
+    path('view_post/<int:pk>', BlogPostDetailView.as_view(), name='view_post'),
+    path('delete_post/<int:pk>', BlogPostDeleteView.as_view(), name='delete_post'),
+]
