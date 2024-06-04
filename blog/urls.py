5e064064e487ce55a804a13bf01c5d940578c1d5
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-04 10:37:53 +0500

    Add cache_page

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
 from blog.views import BlogPostCreateView, BlogPostUpdateView, BlogPostDetailView, BlogPostDeleteView
@@ -8,6 +9,6 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('create_post', BlogPostCreateView.as_view(), name='create_post'),
     path('edit_post/<int:pk>', BlogPostUpdateView.as_view(), name='edit_post'),
-    path('view_post/<int:pk>', BlogPostDetailView.as_view(), name='view_post'),
+    path('view_post/<int:pk>', cache_page(60)(BlogPostDetailView.as_view()), name='view_post'),
     path('delete_post/<int:pk>', BlogPostDeleteView.as_view(), name='delete_post'),
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from main.apps import MainConfig
 from main.views import MainTemplateView
@@ -6,5 +7,5 @@ from main.views import MainTemplateView
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', MainTemplateView.as_view(), name='home_page')
+    path('', cache_page(60)(MainTemplateView.as_view()), name='home_page')
 ]
