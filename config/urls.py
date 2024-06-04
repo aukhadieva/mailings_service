c3fb2454c32b2b93b43776da8515f5219867a26f
Olga Aukhadieva <sarole4ka@gmail.com>
2024-06-04 10:09:32 +0500

    Update path for blog

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('', include('main.urls', namespace='main')),
     path('mailing/', include('mailing.urls', namespace='mailing')),
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('posts/', include('blog.urls', namespace='blog'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
